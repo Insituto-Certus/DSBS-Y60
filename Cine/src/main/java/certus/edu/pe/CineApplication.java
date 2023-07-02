91e0b691ed58ea5e281a05c6ac109013d8fd780d
alexis5999 <alexis5999@gmail.com>
2023-07-01 20:49:52 -0500

    sesión de clases miercoles 26 de Junio

--- Cine/src/main/java/certus/edu/pe/CineApplication.java
+++ Cine/src/main/java/certus/edu/pe/CineApplication.java
@@ -2,7 +2,9 @@ package certus.edu.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // habilitar el client
 @SpringBootApplication
 public class CineApplication {
 
--- MS-Productos/src/main/java/certus/edu/pe/MsProductosApplication.java
+++ MS-Productos/src/main/java/certus/edu/pe/MsProductosApplication.java
@@ -3,6 +3,8 @@ package certus.edu.pe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MsProductosApplication {
 
